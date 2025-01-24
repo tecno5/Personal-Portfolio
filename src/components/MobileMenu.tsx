e582b9f4e2eb8f722d63544b8f5d2c19cfb0d3f4
tecno5 <mail4yash@gmail.com>
2025-01-24 13:27:51 -0600

    Prod Adjustments

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,6 +1,25 @@
-<div className="space-y-4">
-  <Link href="/" className="block text-lg">Home</Link>
-  <Link href="/about" className="block text-lg">About</Link>
-  <Link href="/#projects" className="block text-lg">Projects</Link>
-  {/* ... other menu items */}
-</div> 
+import Link from 'next/link';
+
+export default function MobileMenu() {
+  return (
+    <div className="space-y-4">
+      <Link href="/" className="block text-lg">Home</Link>
+      <Link href="/about" className="block text-lg">About</Link>
+      <Link href="/#projects" className="block text-lg">Projects</Link>
+      <Link 
+        href="https://read.cv/yashrao" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="block text-lg"
+      >
+        Resume
+      </Link>
+      <Link 
+        href="mailto:hello@yashrao.dev" 
+        className="block text-lg"
+      >
+        Contact
+      </Link>
+    </div>
+  );
+} 
